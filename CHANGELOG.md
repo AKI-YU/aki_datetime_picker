cdbb2a94f6d641ffa83c1b68c0205ffc44dcc9ad
AKI YU <nobodyyu@gmail.com>
2024-04-30 15:02:54 +0800

    bug fixed

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,2 @@
-## 1.0.0
-
-* Initial Version
-
-## 0.0.2
-* test project
-  
 ## 0.0.1 
 * create project
--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/aki_yu/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/aki_yu/workspace/aki/GitLab/aki_datetime_picker"
+export "FLUTTER_APPLICATION_PATH=/Users/aki_yu/workspace/aki/Github/aki_datetime_picker"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
